79c28af491fcca6c69998dba56b8cb983399ac2e
Steven Kim <swkim86@gmail.com>
2022-12-29 16:51:33 +0900

    1.0.2

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -89,7 +89,7 @@ class FFAppState extends ChangeNotifier {
     _driverLicenseNumber = _value;
   }
 
-  String _apiEndpointTarget = 'dev';
+  String _apiEndpointTarget = 'prod';
   String get apiEndpointTarget => _apiEndpointTarget;
   set apiEndpointTarget(String _value) {
     _apiEndpointTarget = _value;
